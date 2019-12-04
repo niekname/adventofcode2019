41be17ac386547ec1e7d58ba08dc9d46702d65b9
Kaj Van der Hallen <kaj.vanderhallen@gmail.com>
2019-12-04 21:01:14 +0100

    Made elixir tests parameterized

--- elixir/advent_of_code/test/day_1_part_1_test.exs
+++ elixir/advent_of_code/test/day_1_part_1_test.exs
@@ -1,16 +1,10 @@
 defmodule Day1Part1Test do
     use ExUnit.Case
-  
-    test "a module with a mass of 12 needs 2 fuel" do
-        assert Day1Part1.calculateNeededFuelForModule(12) == 2
-    end
 
-    test "a module with a mass of 14 needs 2 fuel" do
-        assert Day1Part1.calculateNeededFuelForModule(14) == 2
-    end
-
-    test "a module with a mass of 1969 needs 654 fuel" do
-        assert Day1Part1.calculateNeededFuelForModule(1969) == 654
+    for {input, output} <- [{12, 2}, {14, 2}, {1969, 654}] do
+        test "a module with a mass of #{input} needs #{output} fuel" do
+            assert Day1Part1.calculateNeededFuelForModule(unquote(input)) == unquote(output)
+        end
     end
 
     test "modules with a mass of 14 and 1969 needs 656 fuel in total" do
--- elixir/advent_of_code/test/day_1_part_2_test.exs
+++ elixir/advent_of_code/test/day_1_part_2_test.exs
@@ -1,19 +1,13 @@
 defmodule Day1Part2Test do
     use ExUnit.Case
-  
-    test "a module with a mass of 12 needs 2 fuel" do
-        assert Day1Part2.calculate(12) == 2
+
+    for {input, output} <- [{12, 2}, {14, 2}, {1969, 966}] do
+        test "a module with a mass of #{input} needs #{output} fuel" do
+            assert Day1Part2.calculate(unquote(input)) == unquote(output)
+        end
     end
 
-    test "a module with a mass of 14 needs 2 fuel" do
-        assert Day1Part2.calculate(14) == 2
-    end
-
-    test "a module with a mass of 1969 needs 654 fuel" do
-        assert Day1Part2.calculate(1969) == 966
-    end
-
-    test "modules with a mass of 14 and 1969 needs 656 fuel in total" do
+    test "modules with a mass of 14 and 1969 needs 2 + 966 fuel in total" do
         assert Day1Part2.calculateNeededFuelForModules([14, 1969]) == 2 + 966
     end
   end
